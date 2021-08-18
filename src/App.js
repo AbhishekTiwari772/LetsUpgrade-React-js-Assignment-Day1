5991903e661c4466714d8b710f7e835ef08e5aa8
Abhishek Tiwari <84613703+AbhishekTiwari772@users.noreply.github.com>
2021-08-18 09:31:25 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './components/Header';
+import Image from './components/Image';
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Image />
+    </div>
+  );
+}
+
+export default App;
